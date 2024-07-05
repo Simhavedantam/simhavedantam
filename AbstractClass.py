6c4f5779136089470b7c93f4352339533174ea3e
Acies-nithin-simha <nithinsimha.vedantam@aciesglobal.com>
2024-07-05 23:28:28 +0530

    Abstract Class

---
+++ AbstractClass.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+PRINT_STATEMENT = "Hello! This is print statement"
+SCAN_STATEMENT = "Enter The Value:"
+
+
+# class PrintClass(ABC):
+#
+#     @abstractmethod
+#     def print_data(self):
+#         pass
+#
+#
+# class ScanClass(ABC):
+#
+#     @abstractmethod
+#     def scan_data(self):
+#         pass
+#
+#
+# class MyClass(PrintClass, ScanClass):
+#     def __init__(self, data):
+#         self.data = data
+#
+#     def print_data(self):
+#         return self.data
+#
+#     def scan_data(self):
+#         return str(input(self.data))
+#
+
+
+class AbstractClass(ABC):
+
+    @abstractmethod
+    def abstract_method(self):
+        pass
+
+
+class PrinterClass(AbstractClass):
+    def __init__(self, data):
+        self.data = data
+
+    def abstract_method(self):
+        return self.data
+
+
+class ScannerClass(AbstractClass):
+    def __init__(self, data):
+        self.data = data
+
+    def abstract_method(self):
+        return input(self.data)
+
+
+printer = PrinterClass(PRINT_STATEMENT)
+scanner = ScannerClass(SCAN_STATEMENT)
+
+print(scanner.abstract_method())
